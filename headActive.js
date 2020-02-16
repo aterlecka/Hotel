6e6a5fe5f77f0af5cfb70dcafcdb6d64fb608722
aterlecka <aleksandra.terlecka1996@gmail.com>
2020-02-16 16:00:36 +0100

    Change view in booking section

---
+++ headActive.js
@@ -0,0 +1,16 @@
+(function ($) {
+    function floatLabel(inputType) {
+        $(inputType).each(function () {
+            let $this = $(this);
+            $this.focus(function () {
+                $this.next().addClass("active");
+            });
+            $this.blur(function () {
+                if ($this.val() === '' || $this.val() === 'blank') {
+                    $this.next().removeClass();
+                }
+            });
+        });
+    }
+    floatLabel(".floatLabel");
+})(jQuery);
