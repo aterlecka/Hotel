3f73e7a889bfdad5aef78f25af71d8d48772e116
aterlecka <aleksandra.terlecka1996@gmail.com>
2020-06-05 22:25:27 +0200

    create section footer and refactor code

--- headActive.js
+++ headActive.js
@@ -14,3 +14,47 @@
     }
     floatLabel(".floatLabel");
 })(jQuery);
+
+
+
+
+function showHideFooterLinks(child) {
+    if ($(document).width() > 980)
+        return;
+
+    let menu = $(child).parent().find('div');
+    if (menu.css('display') === "none") {
+        menu.css('display', 'flex');
+        $(child).find('.fontello-icon').css('transform', 'rotate(-90deg)')
+    } else {
+        menu.css('display', 'none');
+        $(child).find('.fontello-icon').css('transform', 'rotate(90deg)')
+    }
+}
+
+function showHideHeaderLinks() {
+    if ($(document).width() > 980)
+        return;
+    let x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+const card = document.querySelector('.card'),
+    input = document.querySelector('.input'),
+    line2 = document.querySelector('.line2');
+document.querySelector('.form').addEventListener('submit', function(e) {
+    input.blur();
+    card.classList.add('saving');
+    e.preventDefault();
+});
+line2.addEventListener('animationend', function(e) {
+    setTimeout(() => {
+        card.classList.add('done');
+    }, 1000);
+});
+
+
